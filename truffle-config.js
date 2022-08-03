15aea70c3dedc93dffce8739f5504d3693ff1688
Hwang Seong Taek <hst007@naver.com>
2022-08-03 22:28:33 +0900

    tether contracts

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
-    network:{
+    networks:{
         development: {
         host: '127.0.0.1:',
         port: '7545',
@@ -10,7 +10,7 @@ module.exports = {
         },
     },
     contracts_directory: './src/contracts/',
-    contracts_build_directory: './src/truffle_abis',
+    contracts_build_directory: './src/truffle_abis/',
     compilers: {
         solc: {
             version: '^0.5.0',
