007096d80152c96dd99bbfcae6b03d27576b933b
Hwang Seong Taek <hst007@naver.com>
2022-08-04 23:49:56 +0900

    RWD, DecenBank commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
 const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
 module.exports = async function (deployer) {
+   // Deploy Tether Contract
    await deployer.deploy(Tether)
+
+   // Deploy RWD Contract
+   await deployer.deploy(RWD)
+
+   // Deploy Decentral Bank Contract
+   await deployer.deploy(DecentralBank)
 };
