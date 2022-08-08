dc671ebffa40c68c63a600e5824aa64d51cc7e68
Hwang Seong Taek <hst007@naver.com>
2022-08-08 16:46:37 +0900

    react, navbar

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import './App.css';
+import Navbar from './Navbar';
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            account: '0x011'
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar account={this.state.account}/>
+                <div className='text-center'>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
+
