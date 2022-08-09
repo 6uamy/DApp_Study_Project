74c1d1dc352f5adea0951ce63ebc0d004e25f54d
Hwang Seong Taek <hst007@naver.com>
2022-08-09 21:45:54 +0900

    interactive frontend & blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Web3 from 'web3';
 import Tether from '../truffle_abis/Tether.json';
 import RWD from '../truffle_abis/RWD.json';
 import DecentralBank from '../truffle_abis/DecentralBank.json';
-
+import Main from './Main.js';
 
 class App extends Component {
 
@@ -14,6 +14,7 @@ class App extends Component {
         await this.loadBlockchainData()
     }
 
+    // web3 연결
     async loadWeb3() {
         if(window.ethereum) {
             window.web3 = new Web3(window.ethereum)
@@ -40,7 +41,6 @@ class App extends Component {
             this.setState({tether})
             let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
             this.setState({tetherBalance: tetherBalance.toString()})
-            console.log({tetherBalance: tetherBalance.toString()})
         } 
         else {
             window.alert('Error! Tether contract not deployed!')
@@ -53,7 +53,6 @@ class App extends Component {
             this.setState({rwd})
             let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
             this.setState({rwdBalance: rwdBalance.toString()})
-            console.log({rwdBalance: rwdBalance})
         } 
         else {
             window.alert('Error! RWD contract not deployed!')
@@ -66,7 +65,6 @@ class App extends Component {
             this.setState({decentralBank})
             let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
             this.setState({stakingBalance: stakingBalance.toString()})
-            console.log({stakingBalance: stakingBalance})
         } 
         else {
             window.alert('Error! DecentralBank contract not deployed!')
@@ -76,6 +74,24 @@ class App extends Component {
         this.setState({loading: false})
     }
     
+    // Staking tokens
+    stakeTokens = (amount) => {
+        this.setState({loading: true})
+        this.state.tether.methods.approve(this.state.decentralBank._address, amount).send({from: this.state.account}).on('transactionHash', (hash) => {
+        this.state.decentralBank.methods.depositTokens(amount).send({from: this.state.account}).on('transactionHash', (hash) => {
+            this.setState({loading: false})
+        })
+    })
+    }
+
+    // Unstaking tokens
+    unstakeTokens = () => {
+        this.setState({loading: true})
+        this.state.decentralBank.methods.unstakeTokens().send({from: this.state.account}).on('transactionHash', (hash) => {
+            this.setState({loading: false})
+        })
+    }
+
     constructor(props) {
         super(props)
         this.state = {
@@ -91,13 +107,29 @@ class App extends Component {
     }
 
     render() {
+        let content
+        // this.state.loading ? true일 경우 로딩 텍스트 false일 경우에 메인컨텐츠
+        {this.state.loading ? content = 
+        <p id='loader' className='text-center' style={{margin:'30px'}}>
+        LOADING PLEASE...</p> : content = 
+        <Main
+        tetherBalance = {this.state.tetherBalance}
+        rwdBalance = {this.state.rwdBalance}
+        stakingBalance = {this.state.stakingBalance}
+        stakeTokens = {this.stakeTokens}
+        unstakeTokens = {this.unstakeTokens}
+        />}
         return (
             <div>
                 <Navbar account={this.state.account}/>
-                <div className='text-center'>
-                <h1>
-                    {console.log(this.state.loading)}
-                </h1>
+                <div className='container-fluid mt-5'>
+                    <div className='row'>
+                        <main role='main' className='col-lg-12 ml-auto mr-auto' style={{maxWidth:'600px', minHeight:'100vm'}}>
+                            <div>
+                                {content}
+                            </div>
+                        </main>
+                    </div>
                 </div>
             </div>
         )
