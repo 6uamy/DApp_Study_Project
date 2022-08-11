b66e3e9b0512c434e3cd121eb204fea8052220db
Hwang Seong Taek <hst007@naver.com>
2022-08-12 00:15:42 +0900

    revise spell

--- scripts/issue-tokens.js
+++ scripts/issue-tokens.js
@@ -1,6 +1,6 @@
 const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function issuRewards(callback) {
+module.exports = async function issueRewards(callback) {
     let decentralBank = await DecentralBank.deployed()
     await decentralBank.issueTokens()
     console.log('Tokens have been issued successfully!')
